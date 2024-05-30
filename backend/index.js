c348f0d6f461503d6f309cd692c8e3b5be95327d
DivyamaniNidhi <divyamaninidhi04@gmail.com>
2024-05-30 16:47:53 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 // Load environment variables from .env file
@@ -7,6 +8,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
